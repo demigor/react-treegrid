72f6446453359cd2f4d5a5b683634045434cb30b
demigor <lex@lavnikov.com>
2018-12-20 10:38:57 +0100

    react-virtualized example by default

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-//import Example from './Example-virtualized'
-import Example from './Example-window'
+import Example from './Example-virtualized'
+//import Example from './Example-window'
 
 ReactDOM.render(<Example />, document.getElementById('root'))
